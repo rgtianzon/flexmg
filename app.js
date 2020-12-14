d1e7f923f96e3c93c56672fd0093c8e8a87106e6
Rupert Tianzon <rupert.tianzon@EXTENDBPO.LOCAL>
2020-12-14 06:35:09 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
+const session = require('express-session');
+const methodOverride = require('method-override');
+
+const sessionOptions = { 
+    secret: 'notagoodsecret', 
+    resave: false, 
+    saveUninitialized: false,
+    cookie: {
+        httpOnly: true,
+        expires: Date.now() + 1000 * 60 * 120,
+        maxAge: 1000 * 60 * 120
+    }
+}
+
+app.engine('ejs', ejsMate)
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
+app.use(session(sessionOptions));
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+    if(req.method==='OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH')
+        return res.status(200).json({});
+    }
+    next();
+})
+// ---------------------------------------------------------------------
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+
+
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log(`port is at ${port}`);
+});
